009b71966e9210a3977de35f2232e7eab1154c64
Alexey Chernykh <alexeyshockman@gmail.com>
2025-11-01 16:52:49 +0300

    add kata Bowling score calculator

---
+++ codewars/5kyu/Bowling score calculator/solution.js
@@ -0,0 +1,36 @@
+function bowlingScore(r) {
+    let acc = 0;
+    let step = 0;
+
+    for (let f = 1; f < 10; f++) {
+        if (r[step] === 10) { //strike!
+            acc += 10 + r[step + 1] + r[step + 2];
+            step++;
+            continue;
+        }
+
+        if (r[step] + r[step + 1] === 10) { //spare!
+            acc += 10 + r[step + 2];
+            step += 2;
+            continue;
+        }
+        acc += r[f * 2 - 2] + r[f * 2 - 1];
+        step += 2;
+
+    }
+
+    for (; step < r.length; step++) {
+        acc += r[step];
+    }
+
+    return acc;
+}
+
+
+console.log('expected 0:', bowlingScore([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]));
+console.log('expected 10:', bowlingScore([1, 2, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]));
+console.log('expected 190:', bowlingScore([9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9]));
+console.log('expected 300:', bowlingScore([10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]));
+console.log('expected 11:', bowlingScore([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 1, 0]));
+console.log('expected 12:', bowlingScore([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 1, 0]));
+console.log('expected 200:', bowlingScore([10, 5, 5, 10, 5, 5, 10, 5, 5, 10, 5, 5, 10, 5, 5, 10]));
