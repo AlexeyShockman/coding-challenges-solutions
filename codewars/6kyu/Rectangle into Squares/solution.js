8fcfd92b4e5c9ade6624c2d3deff7477ae54cec1
Alexey Shockman <alexeyshockman@gmail.com>
2025-11-11 12:18:29 +0300

    Added community solutions to the latest kata

--- codewars/6kyu/Rectangle into Squares/solution.js
+++ codewars/6kyu/Rectangle into Squares/solution.js
@@ -21,11 +21,23 @@ function sqInRect(l, w){
     return squares ;
 }
 
+// Первый вариант я решил накидать для того, чтобы получить максимально быстрое решение без сложностей.
+// Второй рекурсивный вариант просто из спортивного интереса
+
 function sqInRectRecursive(l, w) {
     if (l === w) return null;
     if (l < w) [l, w] = [w, l];
 
     return [w, ...(sqInRectRecursive(w, l - w) || [w])];
+}
+
+// Вот еще вариация с рекурсивным подходом, который я уже с codewars взял:
+function sqInRectRecursive3 (a, b, initial = true){
+    if (a === b) { return initial ? null : [a] }
+    const min = Math.min(a, b)
+    const max = Math.max(a, b)
+
+    return [min, ...sqInRectRecursive3(max - min, min, false)]
 }
 
 
@@ -41,5 +53,3 @@ compareInConsole([3, 2, 1, 1], sqInRectRecursive, [3, 5], testIsActive);
 compareInConsole([14, 6, 6, 2, 2, 2], sqInRectRecursive, [20, 14], testIsActive);
 
 
-// Первый вариант я решил накидать для того, чтобы получить максимально быстрое решение без сложностей.
-// Второй рекурсивный вариант просто из спортивного интереса
