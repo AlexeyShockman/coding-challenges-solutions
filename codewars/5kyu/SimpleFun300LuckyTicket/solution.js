35bef55fa5aadf7a1ca2a1fd2f6b4cbde32c3d1c
Alexey Shockman <alexeyshockman@gmail.com>
2026-04-15 15:13:04 +0300

    fix and optimized 5kyu last kata

--- codewars/5kyu/SimpleFun300LuckyTicket/solution.js
+++ codewars/5kyu/SimpleFun300LuckyTicket/solution.js
@@ -1,7 +1,7 @@
 function luckyTicket(str) {
-
     const charCodeOfA = 'a'.charCodeAt(0);
-    let bestResult = Infinity;
+    let bestResult = str.length;
+    let currentResult = str.length;
     let indexOfSlice = 0;
 
     let leftSideFrequencyDictionary = new Array(26).fill(0); //a-z
@@ -11,32 +11,32 @@ function luckyTicket(str) {
         rightSideFrequencyDictionary[str.charCodeAt(i)-charCodeOfA]++;
     }
 
-    function compareSides(leftArr, rightArr) {
-        let compareResult = 0;
-        for (let i = 0; i < 26; i++) {
-            compareResult+= Math.abs(leftArr[i] - rightArr[i])
-        }
-        return compareResult;
-    }
+    for (indexOfSlice; indexOfSlice < str.length - 1; indexOfSlice++) {
+        const letterIndex = str.charCodeAt(indexOfSlice)-charCodeOfA;
 
-    for (indexOfSlice; indexOfSlice < str.length; indexOfSlice++) {
-        rightSideFrequencyDictionary[str.charCodeAt(indexOfSlice)-charCodeOfA]--;
-        leftSideFrequencyDictionary[str.charCodeAt(indexOfSlice)-charCodeOfA]++;
+        const oldDiff = Math.abs(rightSideFrequencyDictionary[letterIndex] - leftSideFrequencyDictionary[letterIndex]);
 
-        const compareResult = compareSides(leftSideFrequencyDictionary, rightSideFrequencyDictionary);
-        if (bestResult > compareResult) {
-            bestResult = compareResult;
+        rightSideFrequencyDictionary[letterIndex]--;
+        leftSideFrequencyDictionary[letterIndex]++;
+
+        const newDiff = Math.abs(rightSideFrequencyDictionary[letterIndex] - leftSideFrequencyDictionary[letterIndex])
+        currentResult -= (oldDiff - newDiff);
+
+        if (bestResult > currentResult) {
+            bestResult = currentResult;
         }
 
     }
-
+ 
     return bestResult;
 }
 
 console.log(luckyTicket('book'), 'expected: 2');
 console.log(luckyTicket('mewmwe'), 'expected: 0');
 console.log(luckyTicket('bbaaaa'), 'expected: 2');
+console.log(luckyTicket('ww'), 'expected: 0');
 console.log(luckyTicket('abcdefa'), 'expected: 5');
+console.log(luckyTicket('abcdefadabcdefa'), 'expected: 1');
 
 // Тест кейсы:
 //         assert.strictEqual(luckyTicket("book"), 2);
@@ -54,4 +54,7 @@ console.log(luckyTicket('abcdefa'), 'expected: 5');
 Это и будет ответом.
 В своих рассуждениях я не учел, что двигать буквы нельзя, поэтому мой подход изначальный не очень сработал. Переработал
 код так, чтобы он исследовал все возможные точки разделения на два подмножества букв и искал оптимальное.
+
+Подумал, что не стоит на каждую итерацию крутить полный пересчет значения, а достаточно просто понять, как оно изменилось.
+Теперь все работает чуть эффективнее
  */
